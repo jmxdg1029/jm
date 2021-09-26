a6cd558a13b25fa842788adb485b41bd8f220f80
John Michael De Guzman <jmxdg1029@gmail.com>
2021-09-26 03:05:09 -0400

    layout fix

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,7 +7,7 @@ const Skills = () => {
     return (
             <div className="skills-section">
                 <div className="skills-header">
-                    <h1>99</h1>
+                    
                 </div>
             </div>
     );
