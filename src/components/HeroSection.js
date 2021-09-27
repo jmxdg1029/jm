85e0543edb397da24472e5c666efec9dd41b4e50
John Michael De Guzman <jmxdg1029@gmail.com>
2021-09-27 17:37:54 -0400

    new layout in hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,11 +7,14 @@ import image3 from './Images/anim2.jfif';
 import Typewriter from "typewriter-effect";
 import CircleType from 'circletype';
 import logo from './Images/logos/logo-1.png'
+import imgB from './Images/word-bb.png';
+import imgA from './Images/word-b.png';
 
 
 
 
 class HeroSection extends React.Component {   
+    /*
     container = React.createRef();
 
     circletypesnap(){
@@ -23,10 +26,12 @@ class HeroSection extends React.Component {
     componentDidMount(){
         this.circletypesnap()
     }
+    */
+    
         render(){
         return(
             <div className="hero-section animated animatedFadeInUp fadeInUp" >
-                <h2 className="container" ref={this.container}>Here's some curved text flowing counter-clockwise.</h2>
+               {/* <h2 className="container" ref={this.container}>Here's some curved text flowing counter-clockwise.</h2>*/}
                 <div className="hero-content">
                     <div className="number-header">
                         {/*<h1><CountUp end={75}  useEasing={true} duration={3.5}/></h1>*/}
@@ -36,6 +41,7 @@ class HeroSection extends React.Component {
                         <div className="hero-info fade-in-content">
                             <hr/>
                             <h1>Web Programmer + Software Dev</h1>
+                            <img className="bg" src={imgB}/>
                             <p className="desc">
                             <Typewriter  
                             options={{
@@ -74,6 +80,7 @@ class HeroSection extends React.Component {
                     </div>
                     
                     <div className="info-under-header">
+                        <img className="bg-under-info" src={imgA}/>
                         <p>
                         Making websites of festival design level for those who care for lead generation
                         </p>
